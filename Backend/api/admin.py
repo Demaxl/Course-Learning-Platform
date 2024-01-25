dee951075a23cece8c1c3a4832b5fcece48120af
demaxl <aweabdullahi@gmail.com>
2024-01-25 15:11:53 +0100

    Defined user model

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,14 @@
+from typing import Any
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from .models import User
 # Register your models here.
+
+
+@admin.register(User)
+class NewUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ["is_student"]}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ["is_student"]}),)
+
+    list_display = UserAdmin.list_display + ("is_student",)
